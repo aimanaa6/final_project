a67d67e995d6b89d6a7d9f3e53f117221cec9ffd
Kinga Kisielewska <kkisielewska9@gmail.com>
2025-04-19 08:17:44 +0100

    m

--- routes.py
+++ routes.py
@@ -1,18 +1,11 @@
+
+
 from flask import render_template, url_for, request, redirect, session
 import bcrypt
 
-from application import app
-from datetime import datetime
-from application.utilities import get_time_of_day
-from application.fake_data import products, people
-import os
-from application.forms.register_form import RegisterForm
-from application.data_access import add_person, get_people
-
-app = Flask(__name__)
+import app
 
 
-@app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
@@ -20,7 +13,7 @@ def home():
 
 @app.route('/aboutus')
 def welcome():
-    return render_template('aboutus.html',title = 'About Us')
+    return render_template('aboutus.html', title='About Us')
 
 @app.route('/founders')
 def founders():
@@ -47,7 +40,9 @@ def contact():
         email = request.form['email']
         subject = request.form['subject']
         message = request.form['message']
- # Handle the form submission (e.g., save to database, send email, etc.)
+        # Handle the form submission (e.g., save to database, send email, etc.)
+
+        session['show_modal'] = True  # ✅ Set modal trigger for thank_you page
         return redirect(url_for('thank_you'))
     return render_template('contact.html')
 
@@ -55,9 +50,14 @@ def contact():
 def thank_you():
     return render_template('thank_you.html')
 
+
 @app.route('/newjoincommunitypage')
 def newjoincommunitypage():
-    return render_template('newjoincommunitypage.html'
+    if 'username' in session:
+        username = session['username']
+        return render_template('newjoincommunitypage.html', username=username, title='Community Page')
+    return render_template('newjoincommunitypage.html', username=False, title='Community Page')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -86,84 +86,26 @@ def register():
                 'Password': hashed_password
             })
 
-            # Assuming add_person is a function to add the person to a database or another list
             add_person(username, first_name, last_name, email, hashed_password)
+
+            session['show_modal'] = True  # ✅ Set modal trigger for newjoincommunitypage
             return redirect(url_for('newjoincommunitypage'))
 
     return render_template('register.html', form=register_form, title='Add Person', message=error)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     error = ""
-#     register_form = RegisterForm()
-#
-#     if request.method == 'POST':
-#         username = register_form.username.data
-#         first_name = register_form.first_name.data
-#         last_name = register_form.last_name.data
-#         email = register_form.email.data
-#         password = register_form.password.data
-#
-#         if len(username) == 0 or len(first_name) == 0 or len(last_name) == 0 or len(email) == 0 or len(password) == 0:
-#             error = 'Please supply all details'
-#         # include others
-#         else:
-#             people.append({'Username': username, 'Firstname': first_name, 'Lastname': last_name, 'Email': email, 'Password': password})
-#             add_person(username, first_name, last_name, email, password)
-#             return redirect(url_for('newjoincommunitypage'))
-#
-#     return render_template('register.html', form=register_form, title='Add Person', message=error)
-
-@app.route('/newjoincommunitypage')
-def newjoincommunitypage():
-    if 'username' in session:
-        username = session['username']
-        return render_template('newjoincommunitypage.html', username=username, title='Community Page')
-# add a notification of success
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # app.logger.debug("Start of login")
     if request.method == 'POST':
         session['username'] = request.form['username']
-        # app.logger.debug("Username is: " + session['username'])
         session['loggedIn'] = True
         session['role'] = 'admin'
         return redirect(url_for('communitypage'))
     return render_template('loginfailed.html', username=False, title='Login Failed')
 
+
 @app.route('/logout')
 def logout():
-    # remove the username from the session if it is there
     session.pop('username', None)
     session.pop('role', None)
     session['loggedIn'] = False
@@ -180,7 +122,6 @@ def adminlogin():
 
 @app.route('/adminviewsubmissions', methods=['GET', 'POST'])
 def adminviewsubmissions():
-    # select username
     if 'username' in session:
         username = session['username']
         print('You are logged in as Admin')
@@ -196,18 +137,12 @@ def communitypage():
         return render_template('communitypage.html', username=username, title='Community Page')
     return render_template('incorrectdetails.html', username=False, title='Wrong credentials')
 
+
 @app.route('/loginfailed', methods=['GET', 'POST'])
 def loginfailed():
-    # app.logger.debug("Start of login")
     if request.method == 'POST':
         session['username'] = request.form['username']
-        # app.logger.debug("Username is: " + session['username'])
         session['loggedIn'] = True
         session['role'] = 'admin'
         return redirect(url_for('adminviewsubmissions'))
     return render_template('loginfailed.html', title="Login Failed")
-# how do we display the login failed message?
-
-
-
-
