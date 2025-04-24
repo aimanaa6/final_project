9889f3f9ac9d79b3bd7ad56ba3689ea313e91859
Aiman Ahmed <aimanahmed61@gmail.com>
2025-04-25 00:53:01 +0100

    EDIT: amended password back to empty

--- application/register_customer.py
+++ application/register_customer.py
@@ -5,7 +5,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="password",
+        password="",
         database="kaasp"
     )
 # Function to insert a new customer with hashed password
