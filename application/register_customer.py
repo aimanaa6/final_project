b02de3cdc6bd83c80273b487626ec25da123ecdb
Kinga Kisielewska <kkisielewska9@gmail.com>
2025-04-27 18:16:46 +0100

    community picsd

--- application/register_customer.py
+++ application/register_customer.py
@@ -7,7 +7,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",
+        password="password",
         database="kaasp",
         # cursorclass = pymysql.cursors.DictCursor
     )
